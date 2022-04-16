272b881495bf6b852be0ae73ee68d7604cc320e9
ltt <ltt5068@163.com>
2022-04-16 16:09:04 -0400

    rename

--- carn/cut.py
+++ carn/cut.py
@@ -13,8 +13,8 @@ def main():
     filename = cfg.src.split('/')[-1]
     print(filename)
     # cv2.imwrite(f"./{filename}", src[0: 100, 256:356])
-    cv2.imwrite(f"./{filename}", src[110:210, 150:250])
     # cv2.imwrite(f"./{filename}", src[160:260, 10:110])
+    cv2.imwrite(f"./{filename}", src[110:210, 150:250])
 
 if __name__ == "__main__":
     main()
